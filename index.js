4900b93b299be83c2e9d9bef51bd62d0ccbedcdb
QureshiFaisal <85436772+QureshiFaisal@users.noreply.github.com>
2022-10-03 18:42:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function linearSearch(arr, num) {
 console.log(linearSearch([1, 2, 3, 4, 5], 4));
 
 //The above function will go through all elements of the array from index 0 till it finds the match.
+
+
+//Global Linear Search return an array that contains all elemenst at which the key is present in ana array
+
+function globalLinearSearch(arr, key){
+   let result = []
+   for(i=0; i< arr.length; i++){
+      if(arr[i] === key) result.push(arr[i])
+   }
+   return result
+}
+console.log(globalLinearSearch([5,7,3,1,7,1,3,4,5,3,1,4], 1))
